1040d454d4616953f6db1fb38d9c91616dd2f0a8
Adillie4 <73258065+Adillie4@users.noreply.github.com>
2020-11-24 13:40:42 -0600

    Create etsyAsset2.js

---
+++ etsyAsset2.js
@@ -0,0 +1,51 @@
+module.exports = [
+    //DROPDOWN SELCTIONS
+    //Jewelry & Accessories Dropdown
+    { cSa: '@ja', dD: '@acc', dDr: 'Accessories', sDd: '@allAcc' },
+    { cSa: '@ja', dD: '@bags', dDr: 'Bags & Purses', sDd: '@allBags' },
+    { cSa: '@ja', dD: '@neck', dDr: 'Necklaces', sDd: '@allNeck' },
+    { cSa: '@ja', dD: '@ring', dDr: 'Rings', sDd: '@allRing' },
+    { cSa: '@ja', dD: '@ear', dDr: 'Earrings', sDd: '@allEar' },
+    { cSa: '@ja', dD: '@brace', dDr: 'Bracelets', sDd: '@allBrace' },
+    { cSa: '@ja', dD: '@body', dDr: 'Body Jewelry', sDd: '@allBody' },
+    //Clothing & Shoes Dropdown
+    { cSa: '@cls', dD: 'span[id="side-nav-category-link-10924"]', dDr: 'Women', sDd: 'a[id="catnav-l3-10924"]' },
+    { cSa: '@cls', dD: 'span[id="side-nav-category-link-10936"]', dDr: 'Men', sDd: 'a[id="catnav-l3-10936"]' },
+    { cSa: '@cls', dD: 'span[id="side-nav-category-link-10941"]', dDr: 'Kids', sDd: 'a[id="catnav-l3-10941"]' },
+    { cSa: '@cls', dD: 'span[id="side-nav-category-link-10952"]', dDr: 'Bags & Purses', sDd: 'a[id="catnav-l3-10952"]' },
+    //Home & Living Dropdown
+    { cSa: '@hl', dD: 'span[id="side-nav-category-link-10960"]', dDr: 'Home & Living', sDd: 'a[id="catnav-l3-10960"]' },
+    { cSa: '@hl', dD: 'span[id="side-nav-category-link-3288"]', dDr: 'Bath & Beauty', sDd: 'a[id="catnav-l3-3288"]' },
+    { cSa: '@hl', dD: 'span[id="side-nav-category-link-4440"]', dDr: 'Pet Supplies', sDd: 'a[id="catnav-l3-4440"]' },
+    //Wedding & Party Dropdown
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-10984"]', dDr: 'Party Supplies', sDd: 'a[id="catnav-l3-10984"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-10987"]', dDr: 'Paper', sDd: 'a[id="catnav-l3-10987"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-10996"]', dDr: 'Decorations', sDd: 'a[id="catnav-l3-10996"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-11006"]', dDr: 'Gifts & Mementos', sDd: 'a[id="catnav-l3-11006"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-11014"]', dDr: 'Accessories', sDd: 'a[id="catnav-l3-11014"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-11031"]', dDr: 'Clothing', sDd: 'a[id="catnav-l3-11031"]' },
+    { cSa: '@wp', dD: 'span[id="side-nav-category-link-11039"]', dDr: 'Jewelry', sDd: 'a[id="catnav-l3-11039"]' },
+    // //Toys & Entertainment Dropdown
+    { cSa: '@te', dD: 'a[id="catnav-l3-11050"]', dDr: 'Toys & Games', sDd: 'empty' },
+    { cSa: '@te', dD: 'a[id="catnav-l3-11057"]', dDr: 'Electronics & Accessories', sDd: 'empty' },
+    { cSa: '@te', dD: 'a[id="catnav-l3-11064"]', dDr: 'Books', sDd: 'empty' },
+    { cSa: '@te', dD: 'a[id="catnav-l3-11076"]', dDr: 'Movies & Music', sDd: 'empty' },
+    // //Art & Collectibles Dropdown
+    { cSa: '@ac', dD: 'a[id="catnav-l3-119"]', dDr: 'Prints', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-115"]', dDr: 'Photography', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-105"]', dDr: 'Painting', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-128"]', dDr: 'Sculpture', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-104"]', dDr: 'Glass Art', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-75"]', dDr: 'Drawing & Illustration', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-99"]', dDr: 'Mixed Media & Collage', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-82"]', dDr: 'Fiber Arts', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-1612"]', dDr: 'Dolls & Miniatures', sDd: 'empty' },
+    { cSa: '@ac', dD: 'a[id="catnav-l3-67"]', dDr: 'Collectibles', sDd: 'empty' },
+    // //Craft Supplies Dropdown
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-9317"]', dDr: 'Home & Hobby', sDd: 'a[id="catnav-l3-9317"]' },
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-9318"]', dDr: 'Jewelry & Beauty', sDd: 'a[id="catnav-l3-9318"]' },
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-9315"]', dDr: 'Sewing & Fiber', sDd: 'a[id="catnav-l3-9315"]' },
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-11089"]', dDr: 'Papercraft', sDd: 'a[id="catnav-l3-11089"]' },
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-9313"]', dDr: 'Visual Arts', sDd: 'a[id="catnav-l3-9313"]' },
+    { cSa: '@crs', dD: 'span[id="side-nav-category-link-9316"]', dDr: 'Sculpting & Forming', sDd: 'a[id="catnav-l3-9316"]' },
+]

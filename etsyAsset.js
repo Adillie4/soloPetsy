513a7801b252d8cc5e4f3beb4d2763aa3c16f742
Adillie4 <73258065+Adillie4@users.noreply.github.com>
2020-11-24 13:40:40 -0600

    Create etsyAsset.js

---
+++ etsyAsset.js
@@ -0,0 +1,11 @@
+module.exports = [
+    //CATAGORY TABS and DROPDOWN SELCTIONS
+    { cS: '@ja', ReH: 'Jewelry & Accessories', },
+    { cS: '@cls', ReH: 'Clothing & Shoes' },
+    { cS: '@hl', ReH: 'Home & Living' },
+    { cS: '@wp', ReH: 'Wedding & Party' },
+    { cS: '@te', ReH: 'Toys & Entertainment' },
+    { cS: '@ac', ReH: 'Art & Collectibles' },
+    { cS: '@crs', ReH: 'Craft Supplies & Tools' },
+]
+
